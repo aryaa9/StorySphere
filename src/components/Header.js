f7199e12b2e8d7caef0d42d06ea0a8162af673ec
aryaa9 <aryaman.singh214@gmail.com>
2024-01-03 22:25:26 -0500

    bibvivh

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export default function Header() {
+  return (
+    <header>
+      <a href="" className="logo">
+        MyBlog
+      </a>
+      <nav>
+        <a href="">Login</a>
+        <a href="">Register</a>
+      </nav>
+    </header>
+  );
+}
